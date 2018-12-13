b5b5cf6609115f653867acaf20103c8fbf4926df
johnsriot <gr8appskc@gmail.com>
2018-12-13 07:12:15 -0600

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//import { link } from 'gatsby'
+
+const Footer = () => (
+  <footer className="footer">
+    <p>&copy; 2018 JohnsRIoT </p>
+  </footer>
+)
+
+export default Footer
